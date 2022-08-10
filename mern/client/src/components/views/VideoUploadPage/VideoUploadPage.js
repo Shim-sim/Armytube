d27c7ea41e649078006cc64475a454054c55a4c9
Shimsim <shimdb96@gmail.com>
2022-08-10 13:23:48 +0000

    add: 비디오 모델 생성 및 업로드 api

--- mern/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ mern/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
 import axios from 'axios'
+import { useSelector } from 'react-redux'
+
 
 const { TextArea } =Input
 const { Title } = Typography
@@ -19,12 +21,18 @@ const CategoryOptions = [
 	{ value: 3, label: "Pets & Animals"}
 ]
 
-const VideoUploadPage = () => {
+const VideoUploadPage = (props) => {
 	
 	const [VideoTitle, setVideoTitle] = useState('')
 	const [Description, serDescription] = useState('')
 	const [Private, setPrivate] = useState(0)
 	const [Category, setCategory] = useState('Film & Animation')
+
+	const user = useSelector(state => state.user)
+	// 썸네일 state
+	// const [FilePath, setFilePath] = useState('')
+	// const [Duration, setDuration] = useState('')
+	// const [ThumbnailPath, setThumbnailPath] = useState('')
 	
 	const onTitleChange = (e) => {
 		setVideoTitle(e.target.value)
@@ -49,28 +57,78 @@ const VideoUploadPage = () => {
 	}
 		formData.append('file', files[0])
 		
-			axios.post('/api/video/uploadfiles', formData, config)
+			axios.post('/api/video/uploads', formData, config)
 			.then(response => {
 				if (response.data.success) {
-					console.lod(response.data)
+					console.log(response.data) // 성공하면 얻게 될 데이터
+					
+					// 썸네일 부분 ffmpeg sudo 오류로 현재 불가능 나중에 직접해보기
+					
+					// let variable = {
+					// 	url: response.data.filePath,
+					// 	fileName: response.data.fileName
+					// }
+					
+					// // setFilePath(response.data.fileName)
+					
+					// axios.post('/api/video/thumbnail', variable)
+					// .then (response => {
+					// 	if (response.data.success) {
+					// 		console.log(response.data) // 성공하면 얻게 될 데이터
+					
+					//   setDuration(response.data.fileDuration)
+					// 	 setThumbnailPath(response.data.thumbsFilePath)
+					// 	} else {
+					// 		alert('썸네일 생성에 실패했습니다.')
+					// 	}
+					// })
 				} else {
 					alert('비디오 업로드를 실패했습니다.')
 				}
 			})
 	}
+	
+	const onSubmit = (e) => {
+		e.preventDefault();
+		
+		const variables = {
+			writer: user.userData._id,
+			title: VideoTitle,
+			description: Description,
+			privacy: Private,
+			filePath: null, // 썸네일 부분
+			category: Category,
+			duration: null, // 썸네일 부분
+			thumbnail: null // 썸네일 부분
+		}
+		
+		axios.post('/api/video/uploadVideo', variables)
+		.then(response => {
+			if(response.data.success) {
+				message.success('성공적으로 업로드 했습니다.')
+				
+				setTimeout(()=> {
+					props.history.push('/')
+				}, 2200)
+			} else {
+				alert('비디오 업로드에  실패 했습니다.')
+			}
+		})
+	}
+	
 	return (
 		<div style={{ maxWidth:'700px', margin:'2rem auto' }}>
 			<div style={{ textAlign:'center', marginBottom:'2rem' }}>
 				<Title level={2}>Upload Video</Title>
 			</div>
 			
-			<Form>
+			<Form onSubmit={onSubmit}>
 				<div style={{ display:'flex', justifyContent:'space-between' }}>
 							{/* Drop zone */}
 					<Dropzone
 						onDrop={onDrop}
 						multiple={false}
-						maxSize={1000000}
+						maxSize={9000000000}
 					>
 						{({ getRootProps, getInputProps }) => (
 							<div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
@@ -82,9 +140,9 @@ const VideoUploadPage = () => {
 					</Dropzone>
 					
 							{/* Thumbnail */}
-					<div>
-						<img/>
-					</div>
+					{/* <div>
+						<img src={ㅁㄴㅇ} alt="thumbnail"/>
+					</div> */}
 				</div>
 			
 				<br />
@@ -123,7 +181,7 @@ const VideoUploadPage = () => {
 				
 				<br />
 				<br />
-				<Button type="primary" size="large" >
+				<Button type="primary" size="large" onClick={onSubmit}>
 					Submit
 				</Button>
 				
--- mern/server/routes/video.js
+++ mern/server/routes/video.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+
+ const { Video } = require("../models/Video");
+
 const multer = require('multer');
- //const { Video } = require("../models/Video");
+const { auth } = require('../middleware/auth')
+var ffmpeg = require('fluent-ffmpeg')
 
 
 var storage = multer.diskStorage({
@@ -29,8 +33,9 @@ var upload = multer({ storage: storage }).single("file")
 //             Video
 //=================================
 
-router.post("/uploadfiles", (req, res) => {
+router.post("/uploads", (req, res) => {
 
+	// 비디오를 서버에 저장한다.
     upload(req, res, err => {
         if (err) {
             return res.json({ success: false, err })
@@ -39,5 +44,58 @@ router.post("/uploadfiles", (req, res) => {
     })
 
 });
+
+router.post("/uploadVideo", (req, res) => {
+
+   // 비디오 정보들을 저장한다.
+	const video = new Video(req.body)
+	
+	video.save((err, doc) => {
+		if(err) return res.json({ success: false, err })
+		res.status(200).json({ success: true })
+	})
+	
+});
+
+
+
+// //=================================
+// //             thumbnail 추후 local에서 사용해보기
+// //=================================
+
+// router.post("/thumbnail", (req, res) => {
+// 	// 썸네일 생성 하고 비디오 러닝타임도 가져오기
+	
+// 	let thumbsFilePath ="";
+//   let fileDuration ="";
+	
+	
+// 	// 비디오 정보 가져오기
+// 	ffmpeg.ffprobe(req.body.url, function(err, metadata){
+// 		ffmpeg.setFfmpegPath()
+// 		console.dir(metadata);
+// 		console.log(metadata.format.duration);
+// 		fileDuration = metadata.format.duration;
+//    })
+	
+// 	// 썸네일 생성
+// 	ffmpeg(req.body.url)
+// 		.on('filenames', function (filenames) {
+// 				console.log('Will generate ' + filenames.join(', '))
+// 				thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+// 		})
+// 		.on('end', function () {
+// 				console.log('Screenshots taken');
+// 				return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+// 		})
+// 		.screenshots({
+// 				// Will take screens at 20%, 40%, 60% and 80% of the video
+// 				count: 3,
+// 				folder: 'uploads/thumbnails',
+// 				size:'320x240',
+// 				// %b input basename ( filename w/o extension )
+// 				filename:'thumbnail-%b.png'
+// 		});
+// });
 
 module.exports = router;
